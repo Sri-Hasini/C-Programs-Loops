01a901079f3f90d68b26931d6fe2f3038057c3fd
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-01-31 23:35:51 +0530

    Add files via upload

---
+++ 42.perfectsquareroot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{  
+    int x,a;
+    scanf("%d",&x);
+    x%a==0;
+    if(x=a*a)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+}
