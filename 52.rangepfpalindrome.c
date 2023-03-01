b13aa435dd9f9cdd6b17997cad30b79e7716759a
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-03-01 20:40:08 +0530

    Add files via upload

---
+++ 52.rangepfpalindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,i,j,n,r;
+	scanf("%d%d",&a,&b);
+	for( i=a;i<=b;i++)
+	{
+		n=i;
+		int rev=0;
+		  while(n>0)
+	    {
+		    r=n%10;
+		    rev=rev*10+r;
+		    n=n/10;
+	    }
+			if(rev==i)
+			{
+				printf("%d ",i);
+			}
+	}
+}
