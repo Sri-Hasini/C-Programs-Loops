970a3734152df9bf4c7da01aa74b22f732a4b149
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-08-27 22:39:17 +0530

    Add files via upload

---
+++ 80.countofoddbetweenodd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n,count=0;
+    int i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+    if(arr[i]%2==0&&arr[i-1]%2==0&&arr[i+1]%2==0)
+    {
+        count++;
+    } 
+    }
+    printf("%d",count);
+    
+}
