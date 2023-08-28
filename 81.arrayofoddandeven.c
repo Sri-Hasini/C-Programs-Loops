e7c6825c26925e1eddd9c9d98bb03638adfd77d9
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-08-28 20:30:49 +0530

    Add files via upload

---
+++ 81.arrayofoddandeven.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	 int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]%2!=0)
+		{
+			printf("%d ",arr[i]);
+		}
+	
+	}
+	for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+        	printf("%d ",arr[i]);
+		}
+	}
+}
