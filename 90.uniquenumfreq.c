a67316c273dd7d38e82d127738aea8f74fb0e634
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-10-09 23:38:33 +0530

    Add files via upload

---
+++ 90.uniquenumfreq.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int freq[101]={};
+	for(i=0;i<n;i++)
+	{
+		freq[arr[i]]++;
+	}
+	for(i=0;i<101;i++)
+	{
+		if(freq[i]>0)
+		{
+			printf("%d ",i);
+		}
+	}
+}
