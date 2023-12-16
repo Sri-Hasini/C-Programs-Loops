149558e3a2c2885bf40999a9cf4ca48535a0ccc4
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-12-16 23:04:42 +0530

    Add files via upload

---
+++ 119.inputandprint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char n;
+	scanf("%c",&n);
+	if(n==a||n==e||n==i||n==o||n==u||n==A||n==E||n==I||n==O||n==u)
+	{
+		printf("Vowel");
+	}
+	else
+	{
+		printf("Consonant");
+	}
+}
